4b20ad6606a43f10b130c4cc8d04d2e54313d433
shz-cod <web.shahidul.alam@gmail.com>
2022-09-11 22:53:33 +0600

    v1.0

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "./assets/css/Apps.css";
+
+import React from "react";
+import Nav from "./Nav";
+
+export default function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
